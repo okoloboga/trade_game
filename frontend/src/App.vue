89ebce1c7a498e0e95c9955b0ff8be96c9652a8f
okoloboga <okoloboga37@gmail.com>
2025-06-14 09:06:05 +0300

    added TonConnectUIProvider in App vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,20 @@
 <template>
-  <v-app>
-    <DefaultLayout>
-      <router-view />
-    </DefaultLayout>
-    <Notification />
-  </v-app>
+  <TonConnectUIProvider :options="options">
+    <v-app>
+      <DefaultLayout>
+        <router-view />
+      </DefaultLayout>
+      <Notification />
+    </v-app>
+  </TonConnectUIProvider>
 </template>
 
 <script setup>
-import DefaultLayout from '@/layouts/DefaultLayout.vue'
-import Notification from '@/components/common/Notification.vue'
+import { TonConnectUIProvider } from '@townsquarelabs/ui-vue';
+import DefaultLayout from '@/layouts/DefaultLayout.vue';
+import Notification from '@/components/common/Notification.vue';
+
+const options = {
+  manifestUrl: `${import.meta.env.VITE_APP_URL}/manifest.json`,
+};
 </script>
