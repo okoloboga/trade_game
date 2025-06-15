0e9d206374745b5c44234540fd55c4a302dfeaab
okoloboga <okoloboga37@gmail.com>
2025-06-15 17:43:27 +0300

    removed anything , only deposit

--- frontend/src/components/wallet/DepositDialog.vue
+++ frontend/src/components/wallet/DepositDialog.vue
@@ -1,38 +1,22 @@
 <template>
-  <v-dialog :value="modelValue" @update:modelValue="$emit('update:modelValue', $event)" max-width="320">
+  <v-dialog v-model="modelValue" max-width="320">
     <v-card>
       <v-card-title>Deposit Dialog</v-card-title>
       <v-card-text>
-        <p style="color: red;">Rendering DepositDialog!</p> <!-- Для видимости -->
+        <p style="color: red;">Rendering DepositDialog!</p>
         <p>This is a minimal deposit dialog.</p>
       </v-card-text>
       <v-card-actions>
-        <v-btn @click="$emit('update:modelValue', false)">Close</v-btn>
+        <v-btn @click="modelValue = false">Close</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
 <script setup>
-import { watch } from 'vue';
+import { ref } from 'vue';
 
-const props = defineProps({
-  modelValue: { type: Boolean, required: true },
-});
-
-const emit = defineEmits(['update:modelValue']);
-const closeDialog = () => {
-  console.log('[DepositDialog] Emitting close event');
-  emit('update:modelValue', false);
-};
-
-console.log('[DepositDialog] Component mounted, initial modelValue:', props.modelValue);
-
-watch(() => props.modelValue, (newValue, oldValue) => {
-  console.log('[DepositDialog] modelValue changed:', newValue, 'from:', oldValue);
-});
+const modelValue = ref(false);
+defineProps(['modelValue']);
+defineEmits(['update:modelValue']);
 </script>
-
-<style scoped>
-/* Временно уберите стили для теста */
-</style>
