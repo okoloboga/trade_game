bf98527fbfabc5360321061bc92c4986f1c74a22
okoloboga <okoloboga37@gmail.com>
2025-06-25 17:53:53 +0300

    return header..

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -23,32 +23,32 @@ const options = {
 /* Увеличиваем специфичность селектора */
 .v-application .v-container,
 .v-container {
-  padding: 4px !important;
+  padding: 2px !important;
 }
 
 /* Перекрываем медиа-запросы Vuetify */
 @media (min-width: 600px) {
   .v-application .v-container,
   .v-container {
-    padding: 4px !important;
+    padding: 2px !important;
   }
 }
 @media (min-width: 960px) {
   .v-application .v-container,
   .v-container {
-    padding: 4px !important;
+    padding: 2px !important;
   }
 }
 @media (min-width: 1264px) {
   .v-application .v-container,
   .v-container {
-    padding: 4px !important;
+    padding: 2px !important;
   }
 }
 @media (min-width: 1904px) {
   .v-application .v-container,
   .v-container {
-    padding: 4px !important;
+    padding: 2px !important;
   }
 }
 
